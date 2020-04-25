2f1566fbe207f397c86f2af8acb442468130399e
dyans31 <priardyans@gmail.com>
2020-04-25 21:55:15 +0700

    publish file index

---
+++ datacovid.php
@@ -0,0 +1,41 @@
+<?php
+$gPilihNW = $_POST['pilihNW'];
+$gJP = $_POST['JP'];
+$gJD=$_POST['JD'];
+$gJS=$_POST['JS'];
+$gJM=$_POST['JM'];
+$gNO=$_POST['NO'];
+$gNIM=$_POST['NIM'];
+$waktu = date("d-m-Y | H:i:s");
+$nf = "komentar.txt";
+$fp = fopen($nf,"r+");
+$data = fgets ($fp,50);
+$comfile = file($nf);
+rewind($fp);
+fwrite($fp,
+"<table width=500 align=center border=1>
+<h3 align='center'>Data Pemantauan Covid19 wilayah $gPilihNW <br>
+      Per $waktu <br>
+      Nama : $gNO | NIM : $gNIM</h3>
+      <tr>
+        <th>Positif</th>
+        <th>Dirawat</th>
+        <th>Sembuh</th>
+        <th>Meninggal</th>
+      </tr>
+      <tr>
+        <td>$gJP</td>
+        <td>$gJD</td>
+        <td>$gJS</td>
+        <td>$gJM</td>
+      </tr>
+</table>
+<br>");
+for ($i=0;$i<999;$i++) {
+  fwrite ($fp, $comfile[isset($i)]);
+}
+fclose ($fp);
+if ($_POST['tombol_simpan']) {
+header ("location: index.php");
+}
+?>
